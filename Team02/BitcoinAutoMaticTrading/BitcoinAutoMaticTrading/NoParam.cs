a1d6ac2c921cef2a4ceb1ba2b42ce2cadb7dfce7
ParkYongHwan <pypy8171@gmail.com>
2022-02-10 21:04:52 +0900

    UpbitApi 에서 받은 AccessKey, SecretKey 로 계정이 가지고 있는 코인 정보 받아 오도록 구현 Rest  API 사용하여 5종목 1초마다 갱신하도록 구현 - 현재 쓸모없는 코드 있는데 조만간 정리할 것

---
+++ Team02/BitcoinAutoMaticTrading/BitcoinAutoMaticTrading/NoParam.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.IdentityModel.Tokens.Jwt;
+using RestSharp;
+
+namespace BitcoinAutoMaticTrading
+{
+    public class NoParam
+    {
+        private string upbitAccessKey;
+        private string upbitSecretKey;
+
+        private DateTime dt_1970_01_01;
+        private const string baseUrl = "https://api.upbit.com";
+
+        public NoParam(string upbitAccessKey, string upbitSecretKey)
+        {
+            //APIClass에서 받은 키를 입력
+            this.upbitAccessKey = upbitAccessKey;
+            this.upbitSecretKey = upbitSecretKey;
+
+            this.dt_1970_01_01 = new DateTime(1970, 01, 01);
+        }
+
+        public string Get(string path, Method method)
+        {
+            var tokenSb = JWT_NoParam();
+            var token = tokenSb.ToString();
+
+            tokenSb.Clear();
+            tokenSb = null;
+
+            var client = new RestClient(baseUrl);       // RestSharp 클라이언트 생성
+            var request = new RestRequest(baseUrl + path, method);
+            request.AddHeader("Content-Type", "application/json"); // 컨텐츠 타입이 json이라고 서버측에 알려줌
+            request.AddHeader("Authorization", token); // 인증을 위해 JWT토큰을 넘겨줌
+
+            token = null;
+
+            var response = client.ExecuteAsync(request); // 요청을 서버측에 보내 응답을 받음
+
+            response.Wait();
+
+            try
+            {
+                if (response.IsCompletedSuccessfully)
+                {
+                    return response.Result.Content; 
+                }
+                else
+                {
+                    return null; 
+                }
+            }
+            catch
+            {
+                return null; 
+            }
+        }
+
+
+        public StringBuilder JWT_NoParam()
+        {
+            // 이 소스는 
+            // https://docs.upbit.com/docs
+            // 위 링크의 TABLE OF CONTENTS 참조
+            TimeSpan diff = DateTime.Now - dt_1970_01_01;
+            var nonce = Convert.ToInt64(diff.TotalMilliseconds);
+            var payload = new JwtPayload {
+                { "access_key", this.upbitAccessKey },
+                { "nonce",  nonce  }
+            };
+
+            byte[] keyBytes = Encoding.Default.GetBytes(this.upbitSecretKey);
+            var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(keyBytes);
+            var credentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(securityKey, "HS256"); // HMAC SHA256 방식의 줄임말
+            var header = new JwtHeader(credentials);
+            var secToken = new JwtSecurityToken(header, payload);
+
+            var jwtToken = new JwtSecurityTokenHandler().WriteToken(secToken);
+
+            StringBuilder returnStr = new StringBuilder();
+            returnStr.Append("Bearer "); // 띄어쓰기 한칸 있어야함
+            returnStr.Append(jwtToken);
+
+            return returnStr;
+        }
+
+    }
+}
---
+++ Team02/BitcoinAutoMaticTrading/BitcoinAutoMaticTrading/UpbitAPI.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Text;
+using Newtonsoft.Json;
+
+using System.Threading;
+
+
+
+namespace BitcoinAutoMaticTrading
+{
+    public class UpbitAPI
+    {
+        const int INPUT_COUNT = 5;
+
+        private NoParam noparam;
+        private Param param;
+
+        //private List<Account> ListAccount;
+        private List<MarketInfo> m_ListMarketInfo;
+        private List<List<CandleMinute>> m_ListCandleMinutes = new List<List<CandleMinute>>();
+        Dictionary<String, List<CandleMinute>> m_DicCoinCandleMinutes = new Dictionary<String, List<CandleMinute>>();
+
+        public UpbitAPI(string upbitAccessKey, string upbitSecretKey)
+        {
+            noparam = new NoParam(upbitAccessKey, upbitSecretKey);
+            param = new Param(upbitAccessKey, upbitSecretKey);
+        }
+
+        public void SetMarketInfo(List<MarketInfo> ListMarketInfo)
+        {
+            m_ListMarketInfo = ListMarketInfo;
+        }
+
+        public void UpdateCoinInfo(UpbitAPI api)
+        {
+            for (int i = 0; i < m_ListMarketInfo.Count; ++i)
+            {
+                string Str = m_ListMarketInfo[i].market;
+                if (Str.Substring(0, 3) != "KRW") // 원화 마켓에 있는 데이터만 긁어옴
+                    continue;
+
+                if (m_DicCoinCandleMinutes.Count >= INPUT_COUNT)
+                    break;
+
+                m_DicCoinCandleMinutes.Add(m_ListMarketInfo[i].market, api.GetCandleMinutes(m_ListMarketInfo[i].market, UpbitAPI.MinuteUnit._1, DateTime.Now, 1));
+            }
+        }
+        public void print(List<Account> ListAccount)
+        {
+            for (int i = 0; i < ListAccount.Count; ++i) // 해당 계좌가 가지고 있는 코인 정보
+                Console.WriteLine(ListAccount[i].currency + " | " + ListAccount[i].avg_buy_price + " | " + ListAccount[i].unit_currency);
+
+            Console.WriteLine();
+
+            foreach (KeyValuePair<String, List<CandleMinute>> CandleInfo in m_DicCoinCandleMinutes)
+            {
+                Console.WriteLine("코인 :" + CandleInfo.Key + " | 현재 거래 가격 : " + CandleInfo.Value[0].trade_price + " | 거래량 : " + CandleInfo.Value[0].candle_acc_trade_volume + " | 현재 시간 : " + CandleInfo.Value[0].candle_date_time_kst);
+            }
+
+            Console.WriteLine();
+            m_DicCoinCandleMinutes.Clear();
+            //Console.Clear();
+            // }
+            // Console.ReadLine();
+        }
+
+
+        //public void Update()
+        //{
+        //    // 추후엔 버튼을 누르는 등의 input이 있을떄 동작
+        //
+        //    while ()
+        //    {
+        //        for (int i = 0; i < ListAccount.Count; ++i)
+        //            Console.WriteLine(ListAccount[i].currency);
+        //
+        //        Console.WriteLine();
+        //
+        //        for (int i = 0; i < ListMarketInfo.Count; ++i)
+        //            Console.WriteLine(ListMarketInfo[i].korean_name + "-" + ListMarketInfo[i].market + "-");
+        //
+        //        Console.WriteLine();
+        //
+        //        for (int i = 0; i < ListCandleMinutes.Count; ++i)
+        //            Console.WriteLine(ListCandleMinutes[i].market + " | " + ListCandleMinutes[i].trade_price + " | " + ListCandleMinutes[i].candle_date_time_kst);
+        //
+        //        Console.ReadLine();
+        //    }
+        //}
+
+        public List<Account> GetAccount()
+        {
+            var data = noparam.Get("/v1/accounts", RestSharp.Method.Get);
+            return JsonConvert.DeserializeObject<List<Account>>(data); // 하나씩 하려고 하니까 뻑남, JsonConvert.DeserializeObject 확인 해볼 필요 있음
+        }
+
+        public List<MarketInfo> GetMarketInfo()
+        {
+            var data = noparam.Get("/v1/market/all", RestSharp.Method.Get);
+            return JsonConvert.DeserializeObject<List<MarketInfo>>(data);
+        }
+
+        public List<CandleMinute> GetCandleMinutes(string market, MinuteUnit unit, DateTime to = default(DateTime), int count = 1)
+        {
+            // 시세 캔들 조회 - 분(Minute) 캔들
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("market", market);
+            parameters.Add("to", to.ToString("yyyy-MM-dd HH:mm:ss"));
+            parameters.Add("count", count.ToString());
+            var data = param.Get(String.Join("", "/v1/candles/minutes/", (int)unit), parameters, RestSharp.Method.Get);
+            return JsonConvert.DeserializeObject<List<CandleMinute>>(data);
+        }
+
+        public enum MinuteUnit
+        {
+            _1 = 1,
+            _3 = 3,
+            _5 = 5,
+            _10 = 10,
+            _15 = 15,
+            _30 = 30,
+            _60 = 60,
+            _240 = 240
+        }
+    }
+}
