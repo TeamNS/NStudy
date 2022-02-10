c64ce65c4579850da1c848e78690cd514d8b95ce
DESKTOP-LOIDG2I\saem <kingne@naver.com>
2022-02-11 00:56:04 +0900

    Teat Commit

---
+++ Team03/NetCore/NetCore.Data/DataModels/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore.Data.DataModels
+{
+	public class User
+	{
+		public string UserID { get; set; }
+		public string PassWord { get; set; }
+	}
+}
