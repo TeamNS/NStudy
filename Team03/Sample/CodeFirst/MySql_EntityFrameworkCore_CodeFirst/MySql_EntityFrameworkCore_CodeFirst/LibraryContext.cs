b9471af2c2f60348f03b314d5b4efd49536715ac
svrlgt <rpdlaqjrm11@naver.com>
2022-02-13 22:09:03 +0900

    Pomelo EF & MySql EF CodeFirst 샘플 프로젝트 및 테스트 결과

---
+++ Team03/Sample/CodeFirst/MySql_EntityFrameworkCore_CodeFirst/MySql_EntityFrameworkCore_CodeFirst/LibraryContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MySql_Data_EntityFrameworkCore_CodeFirst
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;port=3306;database=commondb;user=root;password=1234;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Publisher>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.ISBN);
+                entity.Property(e => e.Title).IsRequired();
+                entity.HasOne(d => d.Publisher)
+                  .WithMany(p => p.Books);
+            });
+        }
+    }
+}
---
+++ Team03/Sample/CodeFirst/Pomelo_EntityFrameworkCore_MySql_CodeFirst/Pomelo_EntityFrameworkCore_MySql_CodeFirst/LibraryContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pomelo_EntityFrameworkCore_MySql_CodeFirst
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString: @"server=localhost;port=3306;database=commondb;user=root;password=1234;",
+                new MySqlServerVersion(new Version(8, 0, 22)));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Publisher>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.ISBN);
+                entity.Property(e => e.Title).IsRequired();
+                entity.HasOne(d => d.Publisher)
+                  .WithMany(p => p.Books);
+            });
+        }
+    }
+}
